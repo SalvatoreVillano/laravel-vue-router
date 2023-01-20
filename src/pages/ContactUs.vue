6db7e151e0d847a5c4ad1e914f6db2365cc97bf1
SalvatoreVillano <villano.salvatore9@gmail.com>
2023-01-20 16:04:29 +0100

    contact form

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-        Sono ContactUs
+    <div class="row">
+        <div class="col-12">
+            <h1 class="mt-5 mb-3">Contatti</h1>
+            <ContactForm />
+        </div>
     </div>
 </template>
 
 <script>
+import ContactForm from '../components/ContactForm.vue';
 export default {
-    name: 'ContactUs'
+    name: 'ContactUs',
+    components: {
+        ContactForm
+    }
 }
 </script>
 
